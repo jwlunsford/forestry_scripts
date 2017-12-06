b8423419cf638c05642f54361d464a476b865e0a
jwlunsford <jon.lunsford@outlook.com>
2017-12-05 22:53:11 -0600

    added yield financial tool

---
+++ financial_tools/yields.py
@@ -0,0 +1,99 @@
+import math
+import sys
+
+
+def annual_simple_growth(yield1, yield2, periods=5):
+    '''Calculates the annual simple growth rate using
+    yield @ age 1, yield @ age 2, and years in period'''
+    return ((yield2 - yield1) * 100)/(periods * yield1)
+    
+def calc_future_yield(cur_yield, annual_growth, periods=5):
+    '''Calculates the future yield using the annual simple
+    growth rate, the current yield and the number of years in period.'''
+    return cur_yield * (((annual_growth/100) * periods) + 1)
+    
+def annual_compound_growth(future_val, current_val, periods=5):
+    '''Calculates annual compound growth using cuurent and future value.'''
+    annual_exp = 1 / periods
+    return math.pow(future_val/current_val, annual_exp) - 1
+    
+def continue_or_exit():
+    '''Prompts the user to choose between continue or exit.'''
+    cont = input("Enter 1 to continue or 0 to exit > ")
+    if int(cont) == 1:
+        return 'continue'
+    else:
+        return 'quit'
+
+
+if __name__ == '__main__':
+    choice = None
+    while choice != 'quit':
+        print("Select a calculation...\n")
+        calc_choice = input(
+            "S) simple annual " 
+            "growth, F) future yield, C) compound annual growth, "
+            "Q) quit > ")
+              
+        if calc_choice.lower().strip() == "s":
+            # Calculate Simple Annual Growth
+            print("This tool calculates the Annual Simple Growth rate of "
+                "forest yields.")
+            print("- " * 20)
+            yield1 = input("Enter the Yield (e.g.; tons/acre) at time 1 > ")
+            yield2 = input("Enter the Yield (e.g.; tons/acre) at time 2 > ")
+            if yield2 < yield1:
+                print("Yield at time 2 cannot be less than yield at time 1.")
+                yield2 = input("Enter the Yield at time 2 > ")
+            period_length = input("Enter the number of years from time 1 to "
+                "time 2 > ")
+            simple_growth = annual_simple_growth(float(yield1), float(yield2),
+                                                 float(period_length))
+            print("\n")
+            print("The simple annual growth rate "
+                  "for this scenario is {}%".format(round(simple_growth, 2)))
+                        
+            choice = continue_or_exit()
+            
+        elif calc_choice.lower().strip() == "f":
+            # Calculate Future Value
+            print("This tool estimates the future yield of forest stands, "
+                "given the, current yield and annual growth rate.")
+            print("- " * 20)
+            yield1 = input("Enter the current Yield (e.g.; tons/acre) > ")
+            ag_rate = input("Enter the simple annual growth rate: ")
+            period_length = input("How many years into the future do you "
+                 "want to include? > ")
+            future_yield = calc_future_yield(float(yield1), float(ag_rate),
+                                             float(period_length))
+            print("\n")
+            print("The future yield for this scenario "
+                  " is {} (vol/acre)".format(round(future_yield, 2)))
+                
+            choice = continue_or_exit()
+            
+        elif calc_choice.lower().strip() == "c":
+            # Calculate the compound annual growth
+            print("This tool calculates the Annual Compound Growth rate of "
+                "forest stands.")
+            print("- " * 20)
+            cur_val = input("Enter the current value (value can be dollars, "
+                "tons, diameter, etc.) > ")
+            future_val = input("Enter the future value. > ")
+            period_length = input("How many years into the future do you " 
+                "want to include? > ")
+            compound_growth = annual_compound_growth(float(future_val),
+                                                     float(cur_val),
+                                                     float(period_length))
+            print("\n")
+            print("The annual compound growth for this "
+                  "scenario is {}%".format(round(compound_growth * 100, 2)))
+                
+            choice = continue_or_exit()
+                
+        else:
+            sys.exit()
+        
+            
+            
+            
