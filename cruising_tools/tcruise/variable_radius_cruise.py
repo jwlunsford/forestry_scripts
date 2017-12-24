703c665a6b0b41dd2e2f6a4113bb1878d1c7b230
jwlunsford <jon.lunsford@outlook.com>
2017-12-24 09:30:58 -0600

    fixed error in stat cals

--- cruising_tools/tcruise/variable_radius_cruise.py
+++ cruising_tools/tcruise/variable_radius_cruise.py
@@ -18,6 +18,7 @@ class PointSample:
     def __init__(self, path):
         self.path = path
         self.dframes = {}
+        self.plots = 0
                 
     
     def run(self):
@@ -30,21 +31,20 @@ class PointSample:
         self._sum_data(self.dframes["calc_df"])
         
         # print the result
-        print("Summary by Species/Product...")        
+        print("Summary by Species and Product ....\n")        
         print("{} \n".format(self.dframes["out_df"]))
         
         
                 
     def stats(self):
         # calculate and return the stats, pass in the calc_df
-        stat_dict = self._calc_stats(self.dframes["calc_df"])
+        stats = self._calc_stats(self.dframes["calc_df"])
         
         # loop over the dictionary and print the stats
-        print("Stats....")
-        for key, value in stat_dict.items():
-            print("{0:<10s}: {1:6.2f}".format(key, value))
-        print("\n")
-        
+        print("Per acre Stats ....\n")
+        for key, value in stats:
+            print("{0:<18s}: {1:6.2f}".format(key, value))
+        print('\n')
               
     
     def _gen_df(self, path_to_csv):
@@ -66,17 +66,24 @@ class PointSample:
             plot_count = 0
             for n in df.PLT.unique():
                 plot_count += 1
+            self.plots = plot_count
                 
             # df references the raw_df, make a copy so it is left unchanged
             calc_df = df.copy()
                 
             # create new data columns on the raw dataframe
-            calc_df.insert(6, "BAT", np.power(calc_df.DBH, 2) * 0.00545415) #BArea
-            calc_df.insert(7, "PAE", (baf/calc_df.BAT))  # Per Acre Expansion
-            calc_df.insert(8, "TPA", calc_df.PAE/plot_count)  # Trees per Acre
-            calc_df.insert(9, "VOL", pmrc_ucp_tons(calc_df.DBH, calc_df.THT)) # Volume/T
-            calc_df.insert(10, "VPA", calc_df.VOL * calc_df.TPA) # Volume per Acre
-            calc_df.insert(11, "BAPA", calc_df.BAT * calc_df.TPA) # Basal Area per Acre
+            # BAT = basal area per tree
+            # PAE = per acre expansion factor
+            # TPA = trees per acre
+            # VOL = volume per tree
+            # VPA = volume per acre
+            # BAPA = basal area per acre
+            calc_df.insert(6, "BAT", np.power(calc_df.DBH, 2) * 0.00545415)
+            calc_df.insert(7, "PAE", (baf/calc_df.BAT))
+            calc_df.insert(8, "TPA", calc_df.PAE) 
+            calc_df.insert(9, "VOL", pmrc_ucp_tons(calc_df.DBH, calc_df.THT))
+            calc_df.insert(10, "VPA", calc_df.VOL * calc_df.TPA)
+            calc_df.insert(11, "BAPA", baf)
                
             self.dframes["calc_df"] = calc_df
             
@@ -95,8 +102,9 @@ class PointSample:
             out_df = df.copy()
             
             # group by products and aggregate per acre values
-            grp = out_df.groupby(["SPP", "PRD"]).sum()
-            grp = grp[["VPA", "TPA", "BAPA"]]  # show only these columns
+            # have to divide these by plot count
+            grp = out_df[["SPP", "PRD", "TPA", "VPA","BAPA"]].groupby(
+                ["SPP", "PRD"]).sum()/self.plots
             
             self.dframes["out_df"] = grp
         except:
@@ -135,6 +143,13 @@ class PointSample:
         # sum VPA2 (this is sumX2)
         sumX2 = grp["VPA2"].sum()
         
+        # summary basal area per acre
+        t_BA = grp["BAPA"].sum() / self.plots
+        
+        # summary trees per acre
+        t_TPA = grp["TPA"].sum() / self.plots
+        
+        # STATS.....
         # calc STD, SX, SE%, CV%
         std = np.power((sumX2 - np.power(sumX, 2)/plt_ct) / (plt_ct - 1), 0.5)
         std_error = std / np.power(plt_ct, 0.5)
@@ -151,9 +166,15 @@ class PointSample:
             
         smp_error = (std_error / meanX) * 100 * t
         
+        ci_upper = meanX + std_error * t
+        ci_lower = meanX - std_error * t
+        
         # return Dict
-        stats = {"MEAN": meanX, "STD": std, "STD_ERR": std_error, "CV": cv_pct,
-                     "SMP_ERR": smp_error}
+        stats = [("Trees/Acre", t_TPA), ("Basal Area/Acre", t_BA),
+                 ("Mean Tons", meanX), ("Upper Tons", ci_upper),
+                 ("Lower Tons", ci_lower), ("Std. Deviation", std),
+                 ("Std. Error", std_error), ("CV%", cv_pct),
+                 ("Sampling Error", smp_error)]
         return stats
         
           
