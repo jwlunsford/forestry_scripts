d01040723bd643b0418d34405e9d5e02c640e29d
Jon Lunsford <jon.lunsford@outlook.com>
2019-03-13 22:02:45 -0500

    Function: added functions for a CLI menu interface and smooth exit.

--- weibull.py
+++ weibull.py
@@ -1,9 +1,11 @@
 import numpy as np
 import scipy.optimize as sp
+from collections import OrderedDict
 import math
+import sys
 
 
-def solve_weibull(c, p1, pq, p93):
+def _solve_weibull(c, p1, pq, p93):
     """recover the weibull parameters.
 
     Args:
@@ -22,7 +24,7 @@ def solve_weibull(c, p1, pq, p93):
     return (a ** 2) + (2 * a * b * (math.gamma(1 + 1 / c))) + ((b**2) * (math.gamma(1 + 2 / c))) - (pq ** 2)
 
 
-def calc_height(si, age):
+def _calc_height(si, age):
     """calculate height of doms using site index ba=25 value
 
     Args:
@@ -35,7 +37,7 @@ def calc_height(si, age):
     return si * (2.14915 * (1 - math.exp(-0.025042 * age))) ** (0.755862)
 
 
-def calc_percentiles(age, tpa, height):
+def _calc_percentiles(age, tpa, height):
     """calculate the diameter percentiles.
 
     Args:
@@ -52,15 +54,66 @@ def calc_percentiles(age, tpa, height):
     return p1, pq, p93
 
 
+def one_thin_harvest():
+    """Complete scenario for one thin plus a harvest."""
+    pass
+
+
+def two_thins_harvest():
+    """Complete scenario for two thins plus a harvest."""
+    pass
+
+
+def harvest():
+    """Complete scenario for harvest only."""
+    pass
+
+
+
+
+def menu_loop():
+    """Program loop with a CLI menu"""
+    choice = True
+    while choice:
+        print('\n**** Pine Plantation Growth & Yield Prediction System ****\n')
+        print('Choose a scenario by entering the number then hit <Return>')
+        for key, value in menu.items():
+            print('\t{}) {}'.format(key, value.__doc__))
+        choice = input('Scenario: ').lower().strip()
+        if choice in menu:
+            menu[choice]()
+        else:
+            print('Invalid choice.  Please try again.\n')
+            menu_loop()
+
+
+
+def quit():
+    """Exit the program"""
+    sys.exit(1)
+
+
+# create a menu heirarchy for the command line interface
+menu = OrderedDict([
+                   ('1', one_thin_harvest),
+                   ('2', two_thins_harvest),
+                   ('3', harvest),
+                   ('4', quit)])
+
+
 
 if __name__ == '__main__':
-    initial_c_value = 3
+    menu_loop()
 
-    ht = calc_height(75, 20)
-    p1, pq, p93 = calc_percentiles(15, 450, ht)
 
+
+    initial_c_value = 3    # initial value needed for the fsolve function
+    ht = _calc_height(si=75, age=20)   # calculate the height from the inputs
+    p1, pq, p93 = _calc_percentiles(15, 450, ht)  # get the diameter percentiles
+
+    # print the results
     # fsolve() finds the root of a nonlinear equation defined by func(x) = 0
     print("height = {}".format(ht))
     print("percentiles = {}, {}, {}".format(p1, pq, p93))
-    print(sp.fsolve(solve_weibull, initial_c_value, args=(p1, pq, p93)))
+    print(sp.fsolve(_solve_weibull, initial_c_value, args=(p1, pq, p93)))
 
